40f00c30fc78cd7a48c0b3edddfab88317a4bc3b
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-10-22 14:01:23 -0400

    manually input test data, come back later to finish scripts and fixtures

--- scripts/create_renewablesLG.py
+++ scripts/create_renewablesLG.py
@@ -3,18 +3,12 @@ import requests
 fixtures = []
 
 renewablesLG = requests.get('https://api.eia.gov/series/?api_key=8e1ad445c4f1cf0f695f035004b477b0&series_id=AEO.2019.LOWMACRO.CNSM_NA_NA_NA_NA_NA_NA_QBTU.A')
+renewablesLG = renewablesLG.json()
+print(renewablesLG['series'][0])
+  
 
-with open('renewablesLG','r') as file:
-  renewable = json.load(file)
   
-for pk, renewables in enumeerate(renewable, start=1):
-  fixture = {
-    "model": 'Renewable_TotalMarketed_LowGrowth',
-    "pk":"pk",
-    "fields": renewables
-  }
+#   fixtures.append(fixture)
   
-  fixtures.append(fixture)
-  
-with open('../fixtures/renewablesLG.json', 'w') as of:
-  json.dumb(fixtures, of, indent=4)
+# with open('../fixtures/renewablesLG.json', 'w') as of:
+#   json.dumb(fixtures, of, indent=4)
