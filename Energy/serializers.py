fe839a31984144e723d66e33153af1ff4a086ead
Yousef M Ahmad <yousef.m.ahmad@outlook.com>
2019-10-22 09:50:57 -0400

    for you john

--- Energy/serializers.py
+++ Energy/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from .models import Renewable_TotalMarketed_LowGrowth, Data
 
-class Renewable_TotalMarketed_LowGrowth_Serializer(serializers.ModleSerializer):
+
+class DataSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Data
+    fields = ('year', 'quads')
+
+class Renewable_TotalMarketed_LowGrowth_Serializer(serializers.ModelSerializer):
   data = DataSerializer(many=True)
   class Meta:
     model = Renewable_TotalMarketed_LowGrowth
     fields = ('name', 'units', 'description', 'updated')
     
-class DataSerializer(serializers.ModelSerializer)
-  class Meta:
-    model = Data
-    fields = ('year', 'quads')
--- Energy/views.py
+++ Energy/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
 import requests
+from rest_framework import generics
+from .serializers import Renewable_TotalMarketed_LowGrowth_Serializer, DataSerializer
+from .models import Renewable_TotalMarketed_LowGrowth, Data
+
 
 r = requests.get('https://api.eia.gov/series/?api_key=8e1ad445c4f1cf0f695f035004b477b0&series_id=AEO.2019.LOWMACRO.CNSM_NA_NA_NA_NA_NA_NA_QBTU.A')
 r.json()
 # Create your views here.
+class Renewable_TotalMarketed_LowGrowth_List(generics.ListCreateAPIView):
+  queryset = Renewable_TotalMarketed_LowGrowth.objects.all()
+  serializer_class = Renewable_TotalMarketed_LowGrowth_Serializer
+
+class Renewable_TotalMarketed_LowGrowth_Detail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Renewable_TotalMarketed_LowGrowth.objects.all()
+  serializer_class = Renewable_TotalMarketed_LowGrowth_Serializer
